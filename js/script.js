6b4c72d3bdc9ca97b24f1f563a7ff3807df841b8
Mikhail Rantovich <mrantovich@gmail.com>
2021-07-01 16:34:00 +0700

    Использование howler.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,54 @@
-let maxint = 5;
-let buttonRandom = document.querySelector('.button_random');
+let soundArray = [];
+let soundList = ['../audio/sound-01.mp3', '../audio/sound-02.mp3', '../audio/sound-03.mp3'];
+let maxint = soundList.length;
+
+soundList.forEach(element => soundArray.push(new Howl({
+        src: element
+    })
+));
+
+
+const mainUI = document.querySelector('.main__ui-text');
+const buttonRandom = document.querySelector('.button_random');
+const buttonRepeat = document.querySelector('.button_repeat');
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 };
 
-function playNewRandom () {
-    let num = getRandomInt(maxint);
+let num = getRandomInt(maxint);
+let track = soundArray[num];
+let curTrackName = soundList[num].split('/')[2];
+
+function playNow(norepeat=true) {
+    if (norepeat) {
+        num = getRandomInt(maxint);
+        track = soundArray[num];
+        curTrackName = soundList[num].split('/')[2];
+
+        Howler.stop();
+        track.play();
+    }
+
+    else {
+        track.stop();
+        track.play();
+    }
+
+    
+    mainUI.animate([
+        {opacity: 0},
+        {opacity: 1}
+    ], {
+        duration: 600
+    });
+    mainUI.innerHTML = '<p>Играет: ' + curTrackName + '</p>';
 };
 
 
-buttonRandom.addEventListener('click', playNewRandom);
+
+buttonRandom.addEventListener('click', playNow);
+buttonRepeat.addEventListener('click', () => {playNow(false)});
 
 let about = document.querySelector('.header__menu-link');
 about.addEventListener('click', addActive);
